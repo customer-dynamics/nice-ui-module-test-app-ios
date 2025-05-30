73d37bcf4d6dcc107dce8bc044eaa669e36c4ff9
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2025-05-30 12:54:22 -0600

    Initial Commit

---
+++ SDK Test/SDK_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDK_TestApp.swift
+//  SDK Test
+//
+//  Created by Tyler Hendrickson on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SDK_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
